6958a6b3c65f4347c3f18e2090d90965837b660b
hengseanghak99 <hengseanghak10499@gmail.com>
2024-06-22 17:17:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EmailMe from "./pages/EmailMe";
+import HomePage from "./pages/HomePage";
+import ExEdPage from "./pages/ExEdPage";
+import NotFound from "./pages/NotFound"
+import Blogs from "./pages/Blogs";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact-me" element={<EmailMe />} />
+        <Route path="/exed" element={<ExEdPage />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/*" element={<NotFound/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
